79c0ded618d4993a563373e17be3c91702ab0f9d
alexandramilliron <alexandramilliron@gmail.com>
2020-11-03 17:13:19 -0800

    completed portion of API set-up

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import (Flask, render_template, request, flash, session,
                    redirect)
 from model import connect_to_db
+import os 
 
 app = Flask(__name__)
 app.secret_key = ""
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
 
+API_KEY = os.environ['']
 
 
 
