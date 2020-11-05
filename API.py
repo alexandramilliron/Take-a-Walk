0521d98aa638b59b819a015f3822721fd494548e
alexandramilliron <alexandramilliron@gmail.com>
2020-11-04 21:54:51 -0800

    added yelp_data_api and weather_data_api functions

--- API.py
+++ API.py
@@ -16,3 +16,22 @@ def hiking_data_api(latitude, longitude):
     return data['trails']
 
 
+def yelp_data_api(latitude, longitude):
+    url = 'https://api.yelp.com/v3/businesses/search'
+    headers = {'AUTHORIZATION': 'BEARER '+ YELP_KEY}
+    payload = {'latitude': latitude, 'longitude': longitude}
+    response = requests.get(url, params=payload, headers=headers)
+    data = response.json()
+    return data['businesses']
+
+
+def weather_data_api(latitude, longitude):
+    url = 'https://api.openweathermap.org/data/2.5/onecall'
+    payload = {'appid': WEATHER_KEY, 'lat': latitude, 'lon': longitude, 'exclude': 'minutely', 'units': 'imperial'}
+    response = requests.get(url, params=payload)
+    data = response.json() 
+    return data['current']
+
+# timestamp = int("dtnumber")
+# datetime.utcfromtimestamp(timestamp).strftime(%Y-%m-%d %H:%M:%S)
+
