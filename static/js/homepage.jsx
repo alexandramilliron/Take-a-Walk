54020c1e1583ed773bc0d4d119df78faf7d7c87c
alexandramilliron <alexandramilliron@gmail.com>
2020-11-05 16:49:39 -0800

    created app.css, homepage.jsx, ratings.jsx, saved-walks.jsx

---
+++ static/js/homepage.jsx
@@ -0,0 +1,7 @@
+// navigation bar at top of page 
+    // trails (with reviews attached)
+    // restaurants (with reviews attached) 
+    // "My Reviews"
+    // "My Saved Walks" 
+// login/logout embedded on the right 
+
