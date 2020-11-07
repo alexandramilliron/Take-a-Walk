667eccea341f729e3a9a203362f812782e383374
alexandramilliron <alexandramilliron@gmail.com>
2020-11-06 20:42:49 -0800

    added test code for React Router

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -7,3 +7,23 @@
 // "New Walk" button 
 
 
+function Main() {
+    return (
+        <HashRouter>
+        <div>
+         <h1>Simple SPA</h1>
+            <ul className="header">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/login">Login</NavLink></li>
+              <li><NavLink to="/new-walk">New Walk</NavLink></li>
+            </ul>
+            <div className="content">
+                <Route path="/"/>
+                <Route path="/login" component={Login}/>  
+                <Route path="/new-walk" component={NewWalk}/>  
+            </div>
+        </div>
+        </HashRouter>
+    );
+}
+
