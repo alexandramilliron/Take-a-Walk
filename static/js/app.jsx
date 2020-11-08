e443677e266e1007fe540fa551826b60d53626c9
alexandramilliron <alexandramilliron@gmail.com>
2020-11-07 21:59:37 -0800

    redid routing with HashRouter

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,4 +1,24 @@
-ReactDOM.render(
-    <Navbar/>, 
-    document.getElementById("app")
+"use strict";
+
+function App() {
+  return (
+    <HashRouter>
+      <div>
+        <h2>Welcome to Take a Walk!</h2>
+        <nav className="">
+          <ul className="">
+            <li><Link to={"/"} className="nav-link">Home</Link></li>
+            <li><Link to={"/new-walk"} className="nav-link">New Walk</Link></li>
+            <li><Link to={"/login"} className="nav-link">Login</Link></li>
+          </ul>
+        </nav>
+        <hr/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/new-walk" component={NewWalk}/>
+          <Route exact path="/login" component={Login}/>
+        </Switch>
+      </div>
+    </HashRouter>
   );
+}
