e6762a9f781c3448cc72507ebb9a1fc5e7741efe
alexandramilliron <alexandramilliron@gmail.com>
2020-11-07 22:02:09 -0800

    edited Login component to include simple login form, event handler for submission, and useState hooks for user registration

--- static/js/login.jsx
+++ static/js/login.jsx
@@ -1,7 +1,55 @@
 function Login() {
+
+    const [emailReg, setEmailReg] = useState("");
+    const [usernameReg, setUsernameReg] = useState("");
+    const [passwordReg, setPasswordReg] = useState("");
+  
+    function validateForm() {
+      return email.length > 0 && password.length > 0;
+    }
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+
+      const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username: usernameReg, password: passwordReg, email: emailReg})
+        };
+
+        fetch('/register', requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+  
     return (
-        <div>
-            <p>hi it's the login component</p>
+      <div className="">
+        <div className="registration">
+        <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+            <label>Email</label>
+                <input type="text" 
+                onChange={(event) => {setEmailReg(event.target.value)}}/>
+            <label>Username</label>
+                <input type="text" 
+                onChange={(event) => {setUsernameReg(event.target.value)}}/>
+            <label>Password</label>
+                <input type="text" 
+                onChange={(event) => {setPasswordReg(event.target.value)}}/>
+            <button>Register</button>
+            </form>
         </div>
+
+
+
+        <div className="login">
+        <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="username"/>
+            <input type="text" placeholder="password"/>
+            <button>Login</button>
+            </form>
+        </div>
+      </div>
     );
-}
+  }
