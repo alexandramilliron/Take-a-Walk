4684e3b0bc9a1e8409f3f9dbd2363e7761d20854
alexandramilliron <alexandramilliron@gmail.com>
2020-11-12 22:47:40 -0800

    added getLocation function and lat/long as params on fetch request

--- static/js/new-walk.jsx
+++ static/js/new-walk.jsx
@@ -1,19 +1,44 @@
 "use strict";
 
+
+
+
 function NewWalk() {
+
+  let latitude = ''
+  let longitude = '' 
+
+  function getLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        latitude = position.coords.latitude; 
+        longitude = position.coords.longitude;
+        alert('location is working')
+      });
+    } else {
+      alert('Sorry, geolocation is not available.')
+    };
+  };
+  
+
+
+
+
+
     
-    function fetchWeather() {
-        fetch('/api/weather')
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          alert(`The temperature will be ${data.temp}`);
-        });
-      }
-      return (
-        <button onClick={fetchWeather}>
-            get the temperature
-        </button>
-        );
+  function fetchWeather() {
+    fetch(`/api/weather?latitude=${latitude}&longitude=${longitude}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      alert(`Temperature: ${data.temp}. Feels like: ${data.feels_like}. Description: ${data['weather'][0].description}.`);
+    });
+  }
+    return (
+      <div>
+        <button onClick={fetchWeather}>get the temperature</button>
+        <button onClick={getLocation}>Location Test</button>
+      </div>
+    );
 }
