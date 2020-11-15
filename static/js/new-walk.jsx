80410d76f3b1a13bff8bd522c7c3d5a42b8494eb
alexandramilliron <alexandramilliron@gmail.com>
2020-11-14 23:43:32 -0800

    separated out restaurant functions

--- static/js/new-walk.jsx
+++ static/js/new-walk.jsx
@@ -1,35 +1,9 @@
 "use strict";
 
 
-
-
 function NewWalk(props) {
 
-  const [restList, setRestList] = useState([]);
-  
 
-  function fetchRestaurants() {
-
-    fetch(`/api/restaurants?latitude=${props.latitude}&longitude=${props.longitude}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      const rest_objects = data['businesses']
-      const user_rests = []
-
-      for (const rest of rest_objects) {
-        user_rests.push(
-          <ul>
-            <li>{rest.name}</li>
-            <li>{rest.display_phone}</li>
-            <li>{rest['location'].display_address}</li>
-          </ul>
-        );
-      }
-      setRestList(user_rest);
-    });
-  }
 
 
 
@@ -49,8 +23,7 @@ function NewWalk(props) {
     return (
       <div>
         <button onClick={fetchWeather}>get the temperature</button>
-        <button onClick={fetchRestaurants}>restaurants</button>
-          {restList}
+        <Restaurants latitude={props.latitude} longitude={props.longitude}/>
       </div>
     );
 }
