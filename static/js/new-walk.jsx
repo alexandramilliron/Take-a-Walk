e0708a81529f5ed3d82f6a0022219e7b30b0a9f6
alexandramilliron <alexandramilliron@gmail.com>
2020-11-14 20:06:16 -0800

    added useState hook for restaurant list and for loop to iterate over response object for restaurant name, phone, address

--- static/js/new-walk.jsx
+++ static/js/new-walk.jsx
@@ -5,8 +5,32 @@
 
 function NewWalk(props) {
 
+  const [restList, setRestList] = useState([]);
   
-  
+
+  function fetchRestaurants() {
+
+    fetch(`/api/restaurants?latitude=${props.latitude}&longitude=${props.longitude}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const rest_objects = data['businesses']
+      const user_rests = []
+
+      for (const rest of rest_objects) {
+        user_rests.push(
+          <ul>
+            <li>{rest.name}</li>
+            <li>{rest.display_phone}</li>
+            <li>{rest['location'].display_address}</li>
+          </ul>
+        );
+      }
+      setRestList(user_rest);
+    });
+  }
+
 
 
 
@@ -25,6 +49,8 @@ function NewWalk(props) {
     return (
       <div>
         <button onClick={fetchWeather}>get the temperature</button>
+        <button onClick={fetchRestaurants}>restaurants</button>
+          {restList}
       </div>
     );
 }
