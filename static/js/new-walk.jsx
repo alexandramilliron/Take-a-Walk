26a0f1eed186970fedb82276bde2f3fe7fb045a0
alexandramilliron <alexandramilliron@gmail.com>
2020-11-16 00:06:08 -0800

    added useState hook to track walks and newWalk function to create instance of walk class

--- static/js/new-walk.jsx
+++ static/js/new-walk.jsx
@@ -2,28 +2,52 @@
 
 
 function NewWalk(props) {
-
-
-
-
-
-
-
-
     
-  function fetchWeather() {
-    fetch(`/api/weather?latitude=${props.latitude}&longitude=${props.longitude}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      alert(`Temperature: ${data.temp}. Feels like: ${data.feels_like}. Description: ${data['weather'][0].description}.`);
-    });
-  }
+  
+    const [comp, setComp] = useState(true);
+
+    const [walk, setWalk] = useState(null);
+
+
+    function newWalk() {
+
+        console.log(props.user);
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(props.user)
+            };
+
+        fetch('/new-walk', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+              setWalk(data); 
+            });
+    }
+
+
+    useEffect(() => {
+      newWalk();
+    }, []); 
+
+
+    function fetchWeather() {
+        fetch(`/api/weather?latitude=${props.latitude}&longitude=${props.longitude}`)
+        .then(response => {
+        return response.json();
+        })
+        .then(data => {
+          alert(`Temperature: ${data.temp}. Feels like: ${data.feels_like}. Description: ${data['weather'][0].description}.`);
+        });
+    }
+    
+    
     return (
       <div>
         <button onClick={fetchWeather}>get the temperature</button>
-        <Restaurants latitude={props.latitude} longitude={props.longitude}/>
+        {(comp == true) ? <Restaurants latitude={props.latitude} longitude={props.longitude} setComp={setComp} walk={walk}/> : 
+        <Trails latitude={props.latitude} longitude={props.longitude} walk={walk}/>}
       </div>
     );
 }
