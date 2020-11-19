95781ed0d7cd8059ee39a67b7a83d0c03ff78026
alexandramilliron <alexandramilliron@gmail.com>
2020-11-19 00:12:35 -0800

    added fetchWalkDetails function:

--- static/js/itinerary.jsx
+++ static/js/itinerary.jsx
@@ -1,13 +1,44 @@
 "use strict";
 
-function Itinerary() {
+
+function Itinerary(props) {
+
     const params = useParams();
+
+    const [walkDetails, setWalkDetails] = useState([]);
+
+    function fetchWalkDetails() {
+
+        fetch(`/walk-details?username=${props.user.username}`)
+        .then(response => {
+        return response.json();
+        })
+        .then(data => {
+            const user_walks = data 
+
+            const display_walks = user_walks.map((walk) => { 
+                return (
+                <div key={walk.walk_id}>
+                <ul>
+                    <li>{walk.walk_id}</li>
+                    <li>{walk.walk_date}</li>
+                    <li>{walk.restaurants[0].name}</li> 
+                </ul>
+                </div>);
+            });
+            setWalkDetails(display_walks);
+        }); 
+    };
+
+
+    useEffect(() => {
+        fetchWalkDetails();
+    }, []);
 
 
     return (
         <div>
-            <div>{`Walk ID: ${params.walk_id}`}</div>
-            you've made it to the itinerary my guy 
+            {walkDetails}
         </div>
     )
 }
