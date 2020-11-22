7c7f3957aa1c342385498f47f13655b87b33f56e
alexandramilliron <alexandramilliron@gmail.com>
2020-11-21 16:33:43 -0800

    added notes and /api to fetch request

--- static/js/savedwalks.jsx
+++ static/js/savedwalks.jsx
@@ -6,9 +6,10 @@ function SavedWalks(props) {
     const [walks, setWalks] = useState([]); 
     
 
+
     function fetchWalks() {
 
-        fetch(`/saved-walks?username=${props.user.username}`) 
+        fetch(`/api/saved-walks?username=${props.user.username}`) 
         .then(response => {
         return response.json();
         })
@@ -19,11 +20,21 @@ function SavedWalks(props) {
                 return (
                     <div key={index}>
                         <ul>
-                            <li><Link to={`/itinerary/${walk.walk_id}`}>{`Walk #${index + 1}`}</Link></li>
+                            <li><Link to={`/itinerary/${walk.walk_id}`}>{`Walk #${index + 1}`}</Link></li> 
+                        
                             <li>{`Walk Date: ${walk.walk_date}`}</li>
                         </ul>
                     </div>
                 );
+                // is there a way to send props with a Link? 
+                // options:
+                // fetch all info at once for particular walk and pass it from component to component - /walk-details
+                // fetch only the info you need when you need it ? take id from url and run fetch to get that info 
+                // fetch all info at once and store it in global state? do I need Redux to make that work 
+                // depends on goals
+                // fetches for smaller pieces of data / getting info exactly when you need it 
+                // props for anything it can't get from the parent but otherwise use the data 
+                // if you can pass through the URL - persists the page out of context 
             });
             setWalks(display_walks);
         }); 
