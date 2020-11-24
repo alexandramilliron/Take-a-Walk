a62d8d8d379ec7cb691d4a612d8e47d83ef9cdf8
alexandramilliron <alexandramilliron@gmail.com>
2020-11-24 00:11:28 -0800

    attempted to set variables but need to revisit

--- static/js/Weather.jsx
+++ static/js/Weather.jsx
@@ -3,22 +3,25 @@
 
 function Weather(props) {
 
+    let temperature = '' // figure out variable scoping here 
+
     function fetchWeather() {
         fetch(`/api/weather?latitude=${props.latitude}&longitude=${props.longitude}`)
         .then(response => {
         return response.json();
         })
         .then(data => {
-          const temperature = data.temp;
-          const feels_like =  data.feels_like;
-          const description = data['weather'][0].description; 
+          const temperature = data['current'].temp;
+          const feels_like =  data['current'].feels_like;
+          const description = data['current']['weather'][0].description; 
         });
     }
 
         
     return (
         <div>
-          
+          <button onClick={fetchWeather}>Click to get the weather for this walk!</button>
+            <span>The temperature is {temperature}</span>
         </div>
     );
 }
