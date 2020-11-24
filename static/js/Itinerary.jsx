f3b103c88139a7f5179c2798b90afc5250ae24a5
alexandramilliron <alexandramilliron@gmail.com>
2020-11-24 00:13:17 -0800

    attempted to pull out lat/long but issue with variable scoping

--- static/js/Itinerary.jsx
+++ static/js/Itinerary.jsx
@@ -14,13 +14,21 @@ function Itinerary() {
         return response.json();
         })
         .then(data => {
-            const walk = data 
+            const walk = data
+
+            if (walk.trails.length !== 0) {
+                const latitude = walk.trails[0].latitude // TODO: get latitude and longitude to pass to Weather component
+                const longitude = walk.trails[0].longitude
+            } else {
+                const latitude = walk.restaurants[0].latitude
+                const longitude = walk.restaurants[0].longitude
+            }
 
             const display_walk = 
                 (
                 <div key={walk.walk_id}>
                 <ul>
-                    <li>{walk.walk_date}</li>
+                    <li>{walk.walk_date.slice(0, 16)}</li>
                     {walk.restaurants.map(rest => {
                         return (
                             <li key={rest.rest_id}>{rest.name}</li>
@@ -46,6 +54,7 @@ function Itinerary() {
     return (
         <div>
             {walkDetails}
+            <Weather latitude={latitude} longitude={longitude}/>
         </div>
     )
 }
