0e25572bb086b106e6908207fe945e32fafa6a56
alexandramilliron <alexandramilliron@gmail.com>
2020-11-29 23:26:13 -0800

    created testing directory for pytest

---
+++ tests/functional/test_server_routes.py
@@ -0,0 +1,13 @@
+def test_home_page(client, init_database): # testing with fixture 
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check that the response is valid
+    """
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Take a Walk" in response.data
+    # assert b"Need an account?" in response.data
+    # assert b"Existing user?" in response.data
+
+
