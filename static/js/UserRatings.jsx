ea82360b3bca7b9d3d390d28b3c903ce8bc4befc
alexandramilliron <alexandramilliron@gmail.com>
2020-11-30 23:37:16 -0800

    WIP: added Card for displaying ratings

--- static/js/UserRatings.jsx
+++ static/js/UserRatings.jsx
@@ -2,15 +2,55 @@
 
 function UserRatings(props) {
 
-    fetch(`/api/ratings/${props.user.username}`) 
-    .then(response => {
-        return response.json(); 
-    })
-    .then(data => {
-        console.log(data); 
-    });
+    const [ratingCard, setRatingCard] = useState('');
+
+
+    useEffect(() => {
+        fetchRatings();
+    }, []);
+
+
+    function fetchRatings() {
+
+        fetch(`/api/ratings/${props.user.username}`) 
+        .then(response => {
+            return response.json(); 
+        })
+        .then(data => {
+            const ratings = data 
+
+            const ratingCards = 
+            (
+            <div>
+                {ratings.trail_ratings.map(rating => {
+                    return (
+                        <Accordion defaultActiveKey="0" key={rating.trail_rating_id}>
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} eventKey="0">
+                                    {rating.trail_name}
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>
+                                    You had this to say about this trail: {rating.trail_comment}
+                                    You gave this trail a {rating.trail_star}-star rating.
+                                    You thought the trail was a difficulty level {rating.difficulty_level}.
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        </Accordion>
+                    );
+                })};
+            </div>
+            );
+            setRatingCard(ratingCards);
+        });
+    };
 
     return (
-        <div>You're on the ratings page.</div>
+        <div>You're on the ratings page.
+            {ratingCard}
+        </div>
     );
 }
