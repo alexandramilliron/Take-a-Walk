5a44c0188fe6095da03b5ef08a805c84be252438
alexandramilliron <alexandramilliron@gmail.com>
2020-12-02 12:32:03 -0800

    moved fixtures with sample test data to this file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 from server import app
-from model import connect_to_db, db, User
+from model import connect_to_db, db, Restaurant, RestRating, Trail, TrailRating, User
 import pytest
 
 
@@ -27,6 +27,73 @@ def init_database(client):
     yield  # This is where the testing happens!
 
     db.drop_all()
+    
+
+###############################
+## Fixtures with Sample Data ##
+###############################
 
 
+@pytest.fixture
+def user():
+    user = User(
+        username='newusername', 
+        email='newuseremail@gmail.com', 
+        password='newuserpassword') 
 
+    return user
+
+
+@pytest.fixture
+def restaurant():
+    restaurant = Restaurant(
+        latitude=45,
+        longitude=-122, 
+        name='New Restaurant',
+        price=4,
+        location='Vancouver, WA'
+    )
+
+    return restaurant
+
+
+@pytest.fixture
+def trail():
+    trail = Trail(
+        latitude=45,
+        longitude=-122, 
+        name='New Trail',
+        length=30,
+        location='Vancouver, WA'
+    )
+
+    return trail 
+
+
+@pytest.fixture
+def rest_rating(restaurant, user):
+    rest_rating = RestRating(
+        restaurant=restaurant,
+        user=user,
+        rest_comment='Wow, what a restaurant.',
+        rest_star=5,
+        masks_worn=True,
+        socially_distanced=True,
+        outdoor_seating=True
+    )
+
+    return rest_rating
+
+
+@pytest.fixture
+def trail_rating(trail, user):
+    trail_rating = TrailRating(
+        trail=trail,
+        user=user,
+        trail_comment='Wow, what a trail.',
+        trail_star=5,
+        difficulty_level=3,
+        crowded=False
+    )
+
+    return trail_rating
