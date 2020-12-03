ce48858bbb3d5979702e1cc8b596c82831838a13
alexandramilliron <alexandramilliron@gmail.com>
2020-12-02 23:22:13 -0800

    added restRatingCards and set setRestCards useState

--- static/js/UserRatings.jsx
+++ static/js/UserRatings.jsx
@@ -1,9 +1,10 @@
-"use strict"; 
+"use strict";
+
 
 function UserRatings(props) {
 
-    const [ratingCard, setRatingCard] = useState('');
-
+    const [trailCards, setTrailCards] = useState('');
+    const [restCards, setRestCards] = useState('');
 
     useEffect(() => {
         fetchRatings();
@@ -19,40 +20,76 @@ function UserRatings(props) {
         .then(data => {
             const ratings = data 
 
-            const ratingCards = 
+            const trailRatingCards = 
             (
             <div>
                 {ratings.trail_ratings.map((rating, index) => {
                     return (
                         <Card key={index} style={{ width: '25rem' }}>
                             <Card.Header>
-                                <Link to={`/trails/${rating.trail_id}`}>{rating.trail_name}</Link>
-                                <span className="float-right">
+                                <Link to={`/trails/${rating.trail_id}`}>{rating.trail_name}</Link> <br/>
+                                <span>
                                     {Array.from({length: rating.trail_star}, (value, index) =>
                                     <span key={index}><i className={'fa fa-star checked'}></i></span>)}
                                 </span>
+                                <span className="text-muted">{rating.rated_at}</span>
                             </Card.Header>
-                                <Card.Body>
-                                    
-                                    <Card.Text>
-                                        {rating.rated_at}
-                                        {rating.trail_comment}
-                                    </Card.Text>
-
-                                </Card.Body>
+                            <Card.Body>    
+                                <Card.Text> 
+                                    Difficulty: {rating.difficulty_level} <br/>
+                                    {rating.crowded ? 'This trail was crowded.' : 'This trail was uncrowded.'} <br/>
+                                    "{rating.trail_comment}"
+                                </Card.Text>
+                            </Card.Body>
                         </Card>
                     );
-                })};
+                })}
             </div>
             );
-            setRatingCard(ratingCards);
+
+            const restRatingCards = 
+            (
+            <div>
+                {ratings.restaurant_ratings.map((rating, index) => {
+                    return (
+                        <Card key={index} style={{ width: '25rem' }}>
+                            <Card.Header>
+                                <Link to={`/restaurants/${rating.rest_id}`}>{rating.rest_name}</Link> <br/>
+                                <span>
+                                    {Array.from({length: rating.rest_star}, (value, index) =>
+                                    <span key={index}><i className={'fa fa-star checked'}></i></span>)}
+                                </span>
+                                <span className="text-muted">{rating.rated_at}</span>
+                            </Card.Header>
+                            <Card.Body>    
+                                <Card.Text> 
+                                    {rating.masks_worn ? 'Masks were worn.' : 'Masks weren\'t worn.'} <br/>
+                                    {rating.socially_distanced ? 'Social distancing was enforced.' : 'Social distancing wasn\'t enforced.'} <br/>
+                                    {rating.outdoor_seating ? 'Outdoor seating was available.' : 'There wasn\'t outdoor seating.'} <br/>
+                                    "{rating.rest_comment}"
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    );
+                })}
+            </div>
+            );
+
+            setTrailCards(trailRatingCards);
+            setRestCards(restRatingCards); 
         });
     };
 
     return (
         <div>
-            {props.user.username}'s Ratings
-            {ratingCard}
+            <h2 className="float-left">{props.user.username}'s Ratings</h2>
+            <Container>
+                <Row>
+                    <Col md="auto">{trailCards}</Col>
+                    <Col md="auto">{restCards}</Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </div>
     );
 }
