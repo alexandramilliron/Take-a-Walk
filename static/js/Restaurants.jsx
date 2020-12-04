ea3fe5aad9d2c16d39b7261fcd4cfc385919d490
alexandramilliron <alexandramilliron@gmail.com>
2020-12-03 22:27:17 -0800

    added Media component to display restaurants in database

--- static/js/Restaurants.jsx
+++ static/js/Restaurants.jsx
@@ -15,8 +15,7 @@ function Restaurants() {
             return response.json(); 
         })
         .then(data => {
-            console.log(data);
-
+            
             const rests = data;
 
             const restCards = 
@@ -24,13 +23,28 @@ function Restaurants() {
                 <div>
                     {rests.map((rest, index) => {
                         return (
-                            <Card key={index} style={{ width: '25rem' }}>
-                                <Card.Header><Link to={`/restaurant/${rest.rest_id}`}>{rest.name}</Link></Card.Header>
-                                <Card.Body>
-                                    {rest.phone} <br/>
-                                    {rest.location}
-                                </Card.Body>
-                            </Card> 
+                            <div key={index}>
+                            <Media as='li'>
+                                <Image
+                                    width={64}
+                                    height={64}
+                                    className='mr-3'
+                                    src={rest.image}
+                                />
+                                <Media.Body>
+                                    <h5><Link to={`/restaurant/${rest.rest_id}`}>{rest.name}</Link>{'  '} 
+                                        {rest.avg_star ? Array.from({length: rest.avg_star}, (value, index) =>
+                                        <span key={index}><i className={'fa fa-star checked'}></i></span>) : ''} 
+                                    </h5>
+                                    <div>
+                                        <div>{rest.avg_mask ? `${rest.avg_mask * 100}% of users reported mask usage.` : 'No reviews on masks yet.'}</div>                                       
+                                        <div>{rest.avg_out ? `${rest.avg_out * 100}% of users reported outdoor seating was available.` : 'No reviews on outdoor seating yet.'}</div>                                       
+                                        <div>{rest.avg_soc ? `${rest.avg_soc * 100}% of users reported they were able to socially distance.` : 'No reviews on social distancing yet.'}</div>
+                                    </div>
+                                </Media.Body>
+                            </Media>
+                            <br/>
+                            </div>
                         );
                     })
                     }
