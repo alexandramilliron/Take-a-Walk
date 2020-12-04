53dfd16fcd41f2a211c411211917df07e85d389d
alexandramilliron <alexandramilliron@gmail.com>
2020-12-03 22:27:43 -0800

    added styling

--- static/js/Register.jsx
+++ static/js/Register.jsx
@@ -38,32 +38,46 @@ function Register(props) {
 
 
     return (
-        <div className='registration'>
+        <div>
+            <Container>
+                <Form onSubmit={handleRegistration}>    
+                <Row>
+                    <Col></Col>
+                    <Col md='auto'>
 
-            <h1>Register</h1>
-                <Form onSubmit={handleRegistration}>
-    
-                    <Form.Group controlId='email'>
-                        <Form.Label>email</Form.Label>
-                            <Form.Control placeholder='email' onChange={(event) => {setEmailReg(event.target.value)}}/>
-                    </Form.Group>
-                    <Form.Group controlId='username'>
-                        <Form.Label>username</Form.Label>
-                            <Form.Control placeholder='username' onChange={(event) => {setUsernameReg(event.target.value)}}/>
-                    </Form.Group>
-      
-                    <Form.Group controlId='password'>
-                        <Form.Label>password</Form.Label>
-                            <Form.Control type='password' placeholder='password' onChange={(event) => {setPasswordReg(event.target.value)}}/>
-                    </Form.Group>
-                
-                <ButtonGroup vertical>
-                    <Button id='registerButton' type='submit'>Register</Button>
+                    <div className='center'>
+                        <h1>Register</h1>
+                    </div>
                     <br/>
-                    <Button onClick={() => Login()}>Have an account? Login</Button>
-                </ButtonGroup>
-                
-                </Form>
+    
+                        <Form.Group controlId='email'>
+                            <Form.Label>email</Form.Label>
+                                <Form.Control placeholder='email' onChange={(event) => {setEmailReg(event.target.value)}}/>
+                        </Form.Group>
+
+                        <Form.Group controlId='username'>
+                            <Form.Label>username</Form.Label>
+                                <Form.Control placeholder='username' onChange={(event) => {setUsernameReg(event.target.value)}}/>
+                        </Form.Group>
+        
+                        <Form.Group controlId='password'>
+                            <Form.Label>password</Form.Label>
+                                <Form.Control type='password' placeholder='password' onChange={(event) => {setPasswordReg(event.target.value)}}/>
+                        </Form.Group>
+
+                        <div className='center'>
+                            <ButtonGroup vertical>
+                                <Button id='registerButton' type='submit'>Register</Button>
+                                <br/>
+                                <Button variant="link" onClick={() => Login()}>Already have an account? Login</Button>
+                            </ButtonGroup>
+                        </div>
+
+                    </Col>
+                    <Col></Col>
+                </Row>
+                </Form>  
+            </Container>
         </div>
     );
 }
