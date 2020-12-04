6a6e1849b383d95835a0d77d82baa8ed06bd7b37
alexandramilliron <alexandramilliron@gmail.com>
2020-12-03 17:11:46 -0800

    added Carousel component

--- static/js/Homepage.jsx
+++ static/js/Homepage.jsx
@@ -1,12 +1,5 @@
-"use strict";
+'use strict';
 
-// navigation bar at top of page 
-    // Login/Logout embedded      
-        // if logged in: 
-        // "My Ratings"
-        // "My Saved Walks"
-// brief introduction to site
-// "New Walk" button 
 
 function Home(props) {
 
@@ -19,8 +12,46 @@ function Home(props) {
 
     return (
         <div>
-            <h2>Welcome to Take a Walk!</h2>
+            <Carousel className>
+            <Carousel.Item>
+                <Image
+                className="d-block w-100"
+                src="/static/img/background1.jpg"
+                alt="First slide"
+                width="400px"
+                fluid
+                />
+                <Carousel.Caption>
+                    <h3>Welcome to Take a Walk!</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <Image
+                className="d-block w-100"
+                src="/static/img/background2.jpg"
+                alt="Second slide"
+                fluid
+                />
+                <Carousel.Caption>
+                    <h3></h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <Image
+                className="d-block w-100"
+                src="/static/img/background3.jpg"
+                alt="Third slide"
+                fluid
+                />
+                <Carousel.Caption>
+                    <h3></h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            </Carousel>
+         
+        <div>   
             <p>
+               <i className="fas fa-head-side-mask"></i>
                 It's been (read: continues to be) a tough year. Though it's important to do everything you can to stay safe,
                 you also have to find ways to stay sane. That's where Take a Walk comes in. With Take a Walk, you can 
                 generate an outing near you that includes local walking trails and restaurants with the ADDED BONUS of reviews
@@ -31,6 +62,8 @@ function Home(props) {
                 Let's take a walk! 
             </p>
             <Button onClick={newWalk}>Start a New Walk</Button>
+        </div>
+
         </div>
     );
 }
