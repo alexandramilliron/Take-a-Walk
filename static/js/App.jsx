35550888fd5d636be1760f2f433248c26d2866a1
alexandramilliron <alexandramilliron@gmail.com>
2020-12-04 17:52:36 -0800

    removed some commented-out notes and cleaned up quotes for consistency

--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 
 function App() {
 
 
   function getUser() {
-    return JSON.parse(localStorage.getItem('user')); //opposite of JSON.stringify - turning back into JavaScript object 
+    return JSON.parse(localStorage.getItem('user')); 
   }
 
   const [user, setUser] = useState(getUser);
@@ -16,46 +16,46 @@ function App() {
       <div>
         <Navigation user={user} setUser={setUser}/>
         <Switch>
-          <Route exact path="/">
+          <Route exact path='/'>
             <Home user={user}/>
           </Route>
-          <Route exact path="/new-walk">
+          <Route exact path='/new-walk'>
             <NewWalk user={user}/>
           </Route>
-          <Route exact path="/login">
+          <Route exact path='/login'>
             <Login setUser={setUser}/>
           </Route>
-          <Route exact path="/register">
+          <Route exact path='/register'>
             <Register setUser={setUser}/> 
           </Route>
-          <Route exact path="/saved-walks">
+          <Route exact path='/saved-walks'>
             <SavedWalks user={user}/> 
           </Route>
-          <Route exact path="/itinerary/:walk_id"> 
+          <Route exact path='/itinerary/:walk_id'> 
             <Itinerary user={user}/>
           </Route>
-          <Route exact path="/ratings/:username">
+          <Route exact path='/ratings/:username'>
             <UserRatings user={user}/>
           </Route>
-          <Route exact path="/rest-rating/:rest_id">
+          <Route exact path='/rest-rating/:rest_id'>
             <RestRating user={user}/>
           </Route>
-          <Route exact path="/trail-rating/:trail_id">
+          <Route exact path='/trail-rating/:trail_id'>
             <TrailRating user={user}/>
           </Route>
-          <Route exact path="/trails">
+          <Route exact path='/trails'>
             <Trails user={user}/>
           </Route>
-          <Route exact path="/trail/:trail_id">
+          <Route exact path='/trail/:trail_id'>
             <Trail user={user}/>
           </Route>
-          <Route exact path="/restaurants">
+          <Route exact path='/restaurants'>
             <Restaurants user={user}/>
           </Route>
-          <Route exact path="/restaurant/:rest_id">
+          <Route exact path='/restaurant/:rest_id'>
             <Restaurant user={user}/>
           </Route>
-          <Route path="*">
+          <Route path='*'>
             <PageNotFound user={user}/>
           </Route>
         </Switch>
@@ -64,6 +64,6 @@ function App() {
   );
 }
 
-// on backend, handle not found if it's an API 
+
 
 
