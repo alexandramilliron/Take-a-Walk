546eb268c35975243fa9192f657550fd546844f3
alexandramilliron <alexandramilliron@gmail.com>
2020-12-06 21:56:31 -0800

    added Cards for styling/display and refactored to display walks details

--- static/js/SavedWalks.jsx
+++ static/js/SavedWalks.jsx
@@ -4,27 +4,56 @@
 function SavedWalks(props) {
 
     const [walks, setWalks] = useState([]); 
-    const history = useHistory(); 
     
     function fetchWalks() {
 
-        fetch(`/api/saved-walks?username=${props.user.username}`) 
+        fetch(`/api/saved-walks/${props.user.username}`) 
         .then(response => {
         return response.json();
         })
-        .then(data => {
-            const user_walks = data 
+        .then(data => { 
+            const userWalks = data 
 
-            const display_walks = user_walks.map((walk, index) => {
+            const display_walks = userWalks.map((walk, index) => {
+
+                let latitude = ''
+                let longitude = ''
+
+                if (walk.trails.length !== 0) {
+                    latitude = (walk.trails[0].latitude);
+                    longitude = (walk.trails[0].longitude);
+                } else {
+                    latitude = (walk.restaurants[0].latitude);
+                    longitude = (walk.restaurants[0].longitude);
+                }
+      
                 return (
                     <div key={index}>
-                        <Card className='walk-card' onClick={() => history.push(`/itinerary/${walk.walk_id}`)}>
-                        <Card.Body>
-                            <div><i class="fa fa-street-view" aria-hidden="true"></i>{'  '}
-                            <Link to={`/itinerary/${walk.walk_id}`} className='rating-link'>{`Walk Date: ${walk.walk_date}`}</Link></div>
-                            {/* <div>{`Walk Date: ${walk.walk_date}`}</div>
-                            {`Walk ${index + 1}`} */}
-
+                        <Card className='walk-card'>
+                        <Card.Body style={{ margin: 5, padding: 30, wordSpacing: .08 + 'em' }}>
+                            <Card.Title>
+                            <div><i className='fa fa-calendar' aria-hidden='true'></i>{'  '}
+                                {walk.walk_date}
+                            </div>
+                            </Card.Title>
+                            <Card.Subtitle className='center'>Click on a restaurant or trail to leave a rating.</Card.Subtitle>
+                            <br/>
+                            {walk.restaurants.map(rest => {
+                                return (
+                                    <div key={rest.rest_id}><i className='fa fa-cutlery' aria-hidden='true'></i>{'  '}                                       
+                                        <Link className='walk-link' title='review this restaurant' to={`/rest-rating/${rest.rest_id}/${rest.name}`}>{rest.name}</Link>
+                                    </div> 
+                                );
+                            })}
+                            {walk.trails.map(trail => {
+                                return (
+                                    <div key={trail.trail_id}><i className='fa fa-tree' aria-hidden='true'></i>{'  '}
+                                        <Link className='walk-link' title='review this trail' to={`/trail-rating/${trail.trail_id}/${trail.name}`}>{trail.name}</Link>
+                                    </div>
+                                );
+                            })}
+                            <br/>
+                            <div className='center'><Weather latitude={latitude} longitude={longitude} walk_date={walk.walk_date}/></div>
                         </Card.Body>
                         </Card>
                         <br/>
@@ -46,13 +75,15 @@ function SavedWalks(props) {
         <div className='saved-walks-bg'>
             <Container fluid>
                 <Row>
-                    <Col md={2}>
-                        <h2 className='center choose-h2'>{props.user.username}'s walks:</h2>
+                    <Col md={3}>
+                        <h2 className='choose-h2'>{props.user.username}'s walks:</h2>
                     </Col>
-                    <Col md={4}>
+                </Row>
+                <Row>
+                    <Col md={3}></Col>
+                    <Col md={8}>
                         {walks}
                     </Col>
-                    <Col></Col>
                     <Col></Col>
                 </Row>
             </Container>
