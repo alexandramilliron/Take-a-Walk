d5644acf37fa2ad971c822f3d76f429ef30b102a
alexandramilliron <alexandramilliron@gmail.com>
2020-12-06 21:52:21 -0800

    added username to /saved-walks path

--- static/js/Navigation.jsx
+++ static/js/Navigation.jsx
@@ -26,7 +26,7 @@ function Navigation(props) {
         <Link to={'/restaurants'} className='nav-link'>ALL RESTAURANTS</Link>
         <Link to={'/trails'} className='nav-link'>ALL TRAILS</Link>
         {props.user ? '' : <Link to={'/register'} className='nav-link'>REGISTER</Link>}
-        {props.user ? <Link to={'/saved-walks'} className='nav-link'>MY WALKS</Link> : ''}
+        {props.user ? <Link to={`/saved-walks/${props.user.username}`} className='nav-link'>MY WALKS</Link> : ''}
         {props.user ? <Link to={`/ratings/${props.user.username}`} className='nav-link'>MY RATINGS</Link> : ''}
         {props.user ? <Link to='#' className='nav-link' onClick={handleLogout}>LOGOUT</Link> : <Link to={'/login'} className='nav-link'>LOGIN</Link>}     
     </Nav>
