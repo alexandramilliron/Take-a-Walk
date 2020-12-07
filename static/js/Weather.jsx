e0532d639424eb74af67eb144e572c6e399f7463
alexandramilliron <alexandramilliron@gmail.com>
2020-12-06 21:51:53 -0800

    refactored component to return a button that calls fetch

--- static/js/Weather.jsx
+++ static/js/Weather.jsx
@@ -4,12 +4,8 @@
 function Weather(props) {
 
 
-    useEffect(() => {
-      fetchWeather();
-    }, []);
-
-
     const [weather, setWeather] = useState('');
+    const [open, setOpen] = useState(false); 
 
 
     function fetchWeather() {
@@ -20,13 +16,20 @@ function Weather(props) {
         .then(data => {
           setWeather(`The temperature will be ${data['temp']['day']} but will feel like ${data['feels_like']['day']}.
                       The weather description is "${data['weather'][0]['description']}."`)
+          setOpen(true);
         });
     }
 
     
     return (
         <div>
+
+            {!open ? 
+              <div className='center'><br/> <Button className='roboto-button' variant='outline-secondary' onClick={fetchWeather}>Get the Weather</Button></div> 
+              : ''}
+
           {weather}
+
         </div>
     );
 }
