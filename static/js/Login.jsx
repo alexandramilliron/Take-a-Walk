3518ef2871993e1e6003ea2ab86d76454b814af9
alexandramilliron <alexandramilliron@gmail.com>
2020-12-08 00:15:05 -0800

    added styling with icons and bg image

--- static/js/Login.jsx
+++ static/js/Login.jsx
@@ -9,6 +9,11 @@ function Login(props) {
     const [password, setPassword] = useState('');
 
 
+    function Register() {
+      history.push('/register')
+    }
+
+
     function handleLogin(event) {
       event.preventDefault();
 
@@ -40,25 +45,30 @@ function Login(props) {
             <Row>
               <Col></Col>
 
-              <Col md='auto'>
+              <Col md='auto' className='border rounded-lg' 
+              style={{ padding: 12 + 'em', background: "transparent url('/static/img/loginbg.jpeg') no-repeat center center /cover" }}>
 
               <div className='center'>
-                <h1>Login</h1>
+                <h1 className='choose-h2'>Login</h1>
               </div>
               <br/>
               
               <Form.Group controlId='username'>
-                <Form.Label>username</Form.Label>
+                <Form.Label className='choose-h2'><span className='fa fa-user login-icon fa-lg'></span>{' '}username</Form.Label>
                   <Form.Control type='username' placeholder='username' onChange={(event) => {setUsername(event.target.value)}}/> 
               </Form.Group>
 
               <Form.Group controlId='password'>
-                <Form.Label>password</Form.Label>
+                <Form.Label className='choose-h2'><span className='fa fa-lock login-icon fa-lg'></span>{' '}password</Form.Label>
                   <Form.Control type='password' placeholder='password' onChange={(event) => {setPassword(event.target.value)}}/>
               </Form.Group>
 
               <div className='center'>
-               <Button type='submit' className='roboto-button' variant='secondary'>Login</Button>
+                <ButtonGroup vertical>
+                  <Button type='submit' className='roboto-button' variant='secondary'>Login</Button>
+                  <br/>
+                  <Button className='roboto-button' variant='secondary' onClick={() => Register()}>Want to sign up? Register</Button>
+                </ButtonGroup>
               </div>
 
               </Col>
