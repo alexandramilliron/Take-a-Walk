a55479795bfab3e41b88a720cf34503f620a600b
alexandramilliron <alexandramilliron@gmail.com>
2020-12-09 12:12:25 -0800

    added test_add_walk

--- tests/functional/test_walk_routes.py
+++ tests/functional/test_walk_routes.py
@@ -1,5 +1,5 @@
 import pytest
-from model import Walk 
+from model import Walk, User
 
 
 @pytest.fixture
@@ -7,3 +7,15 @@ def simple_walk(init_database):
     return Walk.query.get(1)
 
 
+@pytest.fixture
+def simple_user(init_database):
+    return User.query.get(1)
+
+
+def test_add_walk(client, simple_user):
+    response = client.post('/api/new-walk', json={        
+        'date': '2020-05-17 00:00:00', 
+        'username': simple_user.username,
+        'user': simple_user.serialize()
+    })
+    assert response.json == {'walk_id': 2}
